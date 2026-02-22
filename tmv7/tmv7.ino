7aae299dac5287c14ffec392343345911aeb6e91
gavc <gavc@oakstar.net>
2026-02-22 19:38:29 +1000

    Bump firmware to 7.1.2 and publish OTA binaries

--- tmv7/tmv7.ino
+++ tmv7/tmv7.ino
@@ -22,11 +22,11 @@ constexpr unsigned long WIFI_BLINK_MS = 250UL;
 constexpr char AP_NAME[] = "TankMonitorAP";
 
 // Update manifest format:
-// version_code=2026022102
-// version_name=7.1.1
-// firmware_url=https://raw.githubusercontent.com/gavc/tmv71/main/ota/tmv7-2026022102.bin
-constexpr long FW_VERSION_CODE = 2026022101;
-constexpr char FW_VERSION_NAME[] = "7.1.0";
+// version_code=2026022201
+// version_name=7.1.2
+// firmware_url=https://raw.githubusercontent.com/gavc/tmv71/main/ota/tmv7-2026022201.bin
+constexpr long FW_VERSION_CODE = 2026022202;
+constexpr char FW_VERSION_NAME[] = "7.1.2";
 constexpr char UPDATE_MANIFEST_URL[] = "https://raw.githubusercontent.com/gavc/tmv71/main/ota/manifest.txt";
 
 constexpr char NTP_SERVER[] = "pool.ntp.org";
